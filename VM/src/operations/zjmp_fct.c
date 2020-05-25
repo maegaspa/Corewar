97319966ec7bbe7fded3a8a17686aa8e897e4eca
maegaspa@student.42lyon.fr <maegaspa@student.42lyon.fr>
2020-05-25 15:41:34 +0200

    FIX ZJUMP w/ verbose

--- VM/src/operations/zjmp_fct.c
+++ VM/src/operations/zjmp_fct.c
@@ -12,32 +12,32 @@
 
 #include "../../includes/corewar.h"
 
-
-
 int			zjmp_fct(t_war *war, t_chariot *chariot)
 {
 	int param;
 	int i;
 
-	param = 0;
 	i = 1;
+	param = ((short)get_2_val(war, chariot, i));
+    param = param % IDX_MOD;
 	if (chariot->carry == 1)
 	{
 		war->back_pc = 1;
-		param = ((short)get_2_val(war, chariot, i));
-		param = param % IDX_MOD;
-        if (war->verbose[2] == 1 && chariot->carry == 1)
+//		param = ((short)get_2_val(war, chariot, i));
+//		param = param % IDX_MOD;
+        if (war->verbose[2] == 1)
         	ft_printf("P %4d | zjmp %d OK\n", (chariot->index + 1), param);
-        else if (war->verbose[2] == 1)
-        {
-        	ft_printf("P %4d | zjmp %d FAILED\n", (chariot->index + 1), param);
-		}
+        print_verbose_16(war, chariot, 3);
 		chariot->pc = ((chariot->pc + param) % MEM_SIZE);
 		if (chariot->pc < 0)
 			chariot->pc += MEM_SIZE;
-		//printf("ZJMP chariot pc = %d\n", chariot->pc);
 	}
 	else
+	{
 		war->back_pc = 0;
+		if (war->verbose[2] == 1)
+			ft_printf("P %4d | zjmp %d FAILED\n", (chariot->index + 1), param);
+		print_verbose_16(war, chariot, 3);
+ 	}
  	return (SUCCESS);
 }
