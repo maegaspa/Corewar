8c48f782d993530d15a133fb6e4028b30dd4abae
maegaspa@student.42lyon.fr <maegaspa@student.42lyon.fr>
2020-06-03 20:32:02 +0200

    auteur + champ

---
+++ zob.s
@@ -0,0 +1,15 @@
+.name "zob"
+.comment "1 semaine sans réponse wola"
+
+nous:	live %1
+
+vico:	zjmp %24
+	zjmp %48
+	zjmp %72
+	zjmp %96
+	zjmp %120
+	zjmp %144
+	zjmp %168
+	zjmp %216
+
+nousfin:	live %-1
